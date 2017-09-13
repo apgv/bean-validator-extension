0e0416b1b88facb4fe51278faa40686479a78590
apgv <ap@foorbar.codes>
2017-09-13 19:47:06 +0200

    Removed JUnit 5 dependencies no longer needed after the 2017.2.4 release of IntelliJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,4 @@ dependencies {
     compile("org.glassfish:javax.el:3.0.1-b08")
     testCompile("org.junit.jupiter:junit-jupiter-api:5.0.0")
     testCompile("org.assertj:assertj-core:3.8.0")
-//    http://junit.org/junit5/docs/current/user-guide/#running-tests-ide-intellij-idea
-//    Only needed to run tests in an IntelliJ IDEA that bundles an older version
-    testRuntime("org.junit.platform:junit-platform-launcher:1.0.0")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.0.0")
-    testRuntime("org.junit.vintage:junit-vintage-engine:4.12.0")
 }
